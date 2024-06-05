6d155f9a24e5fa4b7435ff347e16b9e0715e64de
Varvara <varvara.prokhvatylo@gmail.com>
2024-06-05 23:37:29 +0300

    added counter

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 let productsCountEl = document.getElementById("products-count");
-
 let addToCartBtns = document.querySelectorAll(".add-to-cart");
+
 console.log(productsCountEl);
 console.log(addToCartBtns);
 
 addToCartBtns.forEach((btn) =>
   btn.addEventListener("click", function () {
     console.log("clicked");
+  })
+);
+let counter = 0;
+
+addToCartBtns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    counter++;
+    productsCountEl.innerHTML = counter;
   })
 );
