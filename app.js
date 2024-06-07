95cecfa79454e5377bc00616ba2754603a72cad9
Varvara <varvara.prokhvatylo@gmail.com>
2024-06-07 23:46:15 +0300

    added liked class in css, added js for add-to-cart button and like

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// add to cart
+
 let productsCountEl = document.getElementById("products-count");
 let addToCartBtns = document.querySelectorAll(".add-to-cart");
 
@@ -17,3 +19,43 @@ addToCartBtns.forEach((btn) =>
     productsCountEl.innerHTML = counter;
   })
 );
+
+// like
+
+let likeBtns = document.querySelectorAll(".like");
+console.log(likeBtns);
+
+likeBtns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    console.log("clicked");
+  })
+);
+
+// let style;
+// function getStyleSheet(style) {
+//   for (const sheet of document.styleSheets) {
+//     if (sheet.title === style) {
+//       return sheet;
+//     }
+//   }
+// }
+
+// let backgroundImage = style.icons.like {background-image};
+
+// function changeBtn() {
+//   if (likeBtns === "clicked") {
+//     likeBtns.style.backgroundImage = "url(images/like_hover.png";
+//   } else {
+//     likeBtns.style.backgroundImage = "url(images/like_default.png)";
+//   }
+// }
+
+function changeBtn() {
+  if (likeBtns === likeBtns.forEach) {
+    likeBtns.setAttribute("class", "like-clicked");
+  } else {
+    likeBtns.setAttribute("class", "like");
+  }
+}
+
+likeBtns.forEach((btn) => btn.addEventListener("click", changeBtn));
