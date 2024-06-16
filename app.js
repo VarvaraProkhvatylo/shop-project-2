f6ba8cafc2b5e26b08bf7f5899c78e6c99ce776d
Varvara <varvara.prokhvatylo@gmail.com>
2024-06-16 18:21:51 +0300

    added OOP

--- app.js
+++ app.js
@@ -78,53 +78,53 @@ likeBtns.forEach((btn) =>
   })
 );
 
-/// change product quantity
+/// change product quantity old
 
-const decrementBtns = document.querySelectorAll(".decrement-button")[0];
-const incrementBtns = document.querySelectorAll(".increment-button")[0];
-const inputFields = document.querySelectorAll(".product-quantity input")[0];
+// const decrementBtns = document.querySelectorAll(".decrement-button")[0];
+// const incrementBtns = document.querySelectorAll(".increment-button")[0];
+// const inputFields = document.querySelectorAll(".product-quantity input")[0];
 
-console.log(decrementBtns);
-console.log(incrementBtns);
-console.log(inputFields);
+// console.log(decrementBtns);
+// console.log(incrementBtns);
+// console.log(inputFields);
 
-let currentValue = +inputFields.value;
-toggleDisableState(currentValue);
+// let currentValue = +inputFields.value;
+// toggleDisableState(currentValue);
 
-function toggleDisableState(count) {
-  decrementBtns.disabled = count <= 1;
-  incrementBtns.disabled = count >= 5;
-}
+// function toggleDisableState(count) {
+//   decrementBtns.disabled = count <= 1;
+//   incrementBtns.disabled = count >= 5;
+// }
 
-incrementBtns.addEventListener("click", function () {
-  let currentValue = +inputFields.value;
-  let newValue = currentValue + 1;
-  inputFields.value = newValue;
+// incrementBtns.addEventListener("click", function () {
+//   let currentValue = +inputFields.value;
+//   let newValue = currentValue + 1;
+//   inputFields.value = newValue;
 
-  // if (newValue <= 1) {
-  //   decrementBtns.disabled = true;
-  // } else {
-  //   decrementBtns.disabled = false;
-  // }
+//   // if (newValue <= 1) {
+//   //   decrementBtns.disabled = true;
+//   // } else {
+//   //   decrementBtns.disabled = false;
+//   // }
 
-  /// те саме, що і вище
-  toggleDisableState(newValue);
-});
+//   /// те саме, що і вище
+//   toggleDisableState(newValue);
+// });
 
-decrementBtns.addEventListener("click", function () {
-  let currentValue = +inputFields.value;
-  let newValue = currentValue - 1;
-  inputFields.value = newValue;
+// decrementBtns.addEventListener("click", function () {
+//   let currentValue = +inputFields.value;
+//   let newValue = currentValue - 1;
+//   inputFields.value = newValue;
 
-  // if (newValue <= 1) {
-  //   decrementBtns.disabled = true;
-  // } else {
-  //   decrementBtns.disabled = false;
-  // }
+// if (newValue <= 1) {
+//   decrementBtns.disabled = true;
+// } else {
+//   decrementBtns.disabled = false;
+// }
 
-  /// те саме, що і вище
-  toggleDisableState(newValue);
-});
+/// те саме, що і вище
+//   toggleDisableState(newValue);
+// });
 
 /// add slick
 $(".slider-wrapper").slick({
@@ -132,7 +132,7 @@ $(".slider-wrapper").slick({
   dots: true,
 });
 
-/// More Details homework
+/// More Details homework open/close modal
 let moreDetailsBtns = document.querySelectorAll(".details-btn");
 console.log(moreDetailsBtns);
 
@@ -190,4 +190,120 @@ moreDetailsModal.addEventListener("click", function (e) {
   if (moreDetailsModal === e.target) {
     closeModal();
   }
+  if (e.target === document.querySelector(".form-wrapper h3")) {
+    document.querySelector(".form-wrapper h3").style.color = "red";
+  }
 });
+
+/// Counter OOP - new counter
+
+const decrementBtns = document.querySelectorAll(".decrement-button");
+const incrementBtns = document.querySelectorAll(".increment-button");
+const inputFields = document.querySelectorAll(".product-quantity input");
+
+function Counter(incrementBtn, decrementBtn, inputField) {
+  this.domRefs = {
+    incrementBtn,
+    decrementBtn,
+    inputField,
+  };
+
+  this.toggleDisableState = function () {
+    const count = this.domRefs.inputField.value;
+    this.domRefs.decrementBtn.disabled = count <= 1;
+    this.domRefs.incrementBtn.disabled = count >= 10;
+  };
+
+  this.toggleDisableState();
+  console.log("це this" + this);
+
+  this.decrement = function () {
+    let currentValue = +this.domRefs.inputField.value;
+    let newValue = currentValue - 1;
+    this.domRefs.inputField.value = newValue;
+
+    this.toggleDisableState();
+  };
+
+  this.increment = function () {
+    let currentValue = +this.domRefs.inputField.value;
+    let newValue = currentValue + 1;
+    this.domRefs.inputField.value = newValue;
+
+    this.toggleDisableState();
+  };
+
+  this.domRefs.decrementBtn.addEventListener(
+    "click",
+    this.decrement.bind(this)
+  );
+  this.domRefs.incrementBtn.addEventListener(
+    "click",
+    this.increment.bind(this)
+  );
+}
+
+let counter1 = new Counter(incrementBtns[0], decrementBtns[0], inputFields[0]);
+
+console.log(counter1);
+
+// //// Об'єктно-орієнтовно програмування (OOP) example
+
+// // let audi = {
+// //   model: "A4",
+// //   color: "black",
+// //   year: "2020",
+// // };
+
+// // let lexus = {
+// //   model: "ES350",
+// //   color: "silver",
+// //   year: "2015",
+// // };
+
+// // let bmw = {
+// //   model: "320",
+// //   color: "white",
+// //   year: "2019",
+// // };
+
+// function Car(name, model, color, year) {
+//   this.name = name;
+//   this.model = model;
+//   this.color = color;
+//   this.year = year;
+
+//   this.getCarName = function () {
+//     return this.name + "" + this.model;
+//   };
+// }
+
+// let audi = new Car("Audi", "A4", "black", "2020");
+// let lexus = new Car("Lexus", "ES350", "silver", "2015");
+// let bmw = new Car("BMW", "320", "white", "2019");
+// // console.log(audi);
+// console.log(audi.getCarName());
+// // console.log(lexus);
+// console.log(lexus.getCarName());
+// // console.log(bmw);
+// console.log(bmw.getCarName());
+
+// // new Calculator(a, b); praxis
+
+// function Calculator(a, b) {
+//   this.a = a;
+//   this.b = b;
+
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+
+//   this.mul = function () {
+//     return this.a * this.b;
+//   };
+// }
+
+// let calc1 = new Calculator(2, 10);
+
+// console.log(calc1.sum());
+// console.log(calc1.mul());
